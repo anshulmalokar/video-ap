b43be8fecad5187c463b8cb8e196da5033f5ebee
anshulmalokar <anshulmalokar@gmail.com>
2024-08-24 14:51:59 +0530

    added google oauth

---
+++ client/src/app/_pages/auth.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+type Props = {};
+
+export default function Auth({}: Props) {
+  const { data } = useSession();
+  console.log(data);
+
+  const signOutApp = () => {
+    console.log("Signing out from google");
+    signOut();
+  };
+
+  const signInApp = () => {
+    console.log("Siggning in from google");
+    signIn("google");
+  };
+  return (
+    <>
+      <div className="m-10">
+        <button
+          type="submit"
+          onClick={signInApp}
+          className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          Sign In
+        </button>
+        <button
+          type="submit"
+          onClick={signOutApp}
+          className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          Sign Out
+        </button>
+      </div>
+    </>
+  );
+}
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,11 @@
-import Room from "./pages/room";
-import Upload from "./pages/upload";
+import Auth from "./_pages/auth";
+import Room from "./_pages/room";
+import Upload from "./_pages/upload";
 export default function Home() {
   return (
     <>
        <div className='player-wrapper'>
-        <Upload/>
+        <Auth/>
       </div>
     </>
   );
